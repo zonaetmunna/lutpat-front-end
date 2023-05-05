33f7f509baa3cf43f44272f5fac8c3979d3cd08c
zonaetmunna <zonaetmonna@gmail.com>
2023-05-05 21:43:45 +0600

    add

---
+++ src/view/components/main/Products.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { IProduct } from "../../../types";
+
+const Products = ({ products }: { products: IProduct }) => {
+  console.log(products);
+  return <div></div>;
+};
+
+export default Products;
